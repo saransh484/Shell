34fffd530fc8e4efdf34b3f7ff373dd1755558d7
saransh484 <saransh8@hotmail.com>
2022-11-12 05:14:32 +0530

    some fixes

--- passArgs.sh
+++ passArgs.sh
@@ -10,7 +10,7 @@
 # argument value stored in default variables  namely by index, $1 $2 etc
 # the file name is stored in the variable $0
 
-echo $0 $1 $2 $3 ' > echo $1 $2 $3'
+echo $0 $1 $2 $3 ' > echo $0 $1 $2 $3'
 
 #these variables can be accessed though a array by using $@
 
@@ -18,4 +18,12 @@ argVar=("$@")
 
 # in this array the index[0] is the first argument not the name of the file 
 
-echo "${argVar[1]}"
+echo " argument at index 1 is : ${argVar[1]}"
+
+#Printing whole argument ar once
+
+echo 'total arguments : '$@
+
+# to print the number if arguments entered
+
+echo 'number of arguments : '$# 
